e7507177f865b17ef8a2fd6373cc9a7c216c1127
Christopher <chrisjwkim97@gmail.com>
2023-11-30 10:24:50 -0500

    final

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,18 +1,38 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 function BlogPost({ blog }) {
 
-  const [isRead, setIsRead] = useState(false);
+  const [read, setIsRead] = useState(blog.isRead);
+
   function toggleRead() {
-    setIsRead(!isRead);
+    setIsRead(!read);
   }
+    const newRead = {
+      isRead : read
+    };
+  
+  useEffect(() => {
+    fetch(`http://localhost:4000/blogs/${blog.id}`, {
+      method : "PATCH",
+      headers : {
+        "content-type" : "application/json"
+      },
+      body: JSON.stringify(newRead) 
+    })
+    }, [read])
+    
+    
+    
+    
+  
+  
 
   return (
     <div className="post-container">
       <div className="post-header">
         <div className="head">
           <h3>{blog.title}</h3>
-          <button className="read-button" onClick={toggleRead}>{!isRead ? "Read" : "Unread"}</button>
+          <button className="read-button" onClick={toggleRead}>{!read ? "Read" : "Unread"}</button>
         </div>
         <p>Author {blog.author}</p>
       </div>
